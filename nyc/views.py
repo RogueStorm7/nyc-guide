5030cbb89f0b47b2f0bd2d56ca0ef3eb4273d7e7
storrer <storrer.chris@gmail.com>
2022-04-08 17:42:37 -0700

    Add activity.html template and begin ActivityView class implementation.

--- nyc/views.py
+++ nyc/views.py
@@ -19,6 +19,19 @@ class BoroughView(View):
 
 
 class ActivityView(View):
+    # The borough and activity arguments are strings taken from the URL
+    def get(self, request, borough, activity):
+        # context is a dictionary that will be passed to the render function and will be available in the template
+        context= {
+            'borough': borough, 
+            'activity': activity,
+        }
+
+        return render(
+            request=request,
+            template_name='activity.html',
+            context=context,
+        )
     pass
 
 
