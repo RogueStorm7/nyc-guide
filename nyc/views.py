5d3feeaf530d078fd74f792894abcf0169bbfd60
jose cru <jose227716@gmail.com>
2022-04-10 21:07:24 -0400

    Changes

--- nyc/views.py
+++ nyc/views.py
@@ -21,11 +21,11 @@ class BoroughView(View):
 class ActivityView(View):
     # The borough and activity arguments are strings taken from the URL
     def get(self, request, borough, activity):
-        # context is a dictionary that will be passed to the render function and will be available in the template
+    # context is a dictionary that will be passed to the render function and will be available in the template
         context= {
             'borough': borough, 
             'activity': activity,
-            # Here is the all-important venues data:
+    # Here is the all-important venues data:
             'venues': boroughs[borough][activity].keys(),
         }
         
@@ -35,8 +35,8 @@ class ActivityView(View):
             template_name='activity.html',
             context=context,
         )
-class VenueView(View):
-    pass
+class VenueView(View): 
+    
 #The borough and activity and venue arguments are strings taken from the URL 
     def get (self, request, borough, activity,venue):
 # Return the http response with the rendered template
